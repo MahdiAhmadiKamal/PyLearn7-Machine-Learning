6e6c4b6089e64be199d7f204c94fa6ce8bea1e49
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-07-09 00:43:49 +0430

    5th commit

--- PyLearn7-ML_Mini_Project/train.py
+++ PyLearn7-ML_Mini_Project/train.py
@@ -1,3 +1,29 @@
 import tensorflow as tf
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
+
+# Load Data
+data = pd.read_csv("dataset.csv", header=None)
+
+X = data.iloc[:, :-1].values        # :-1 means upto last column
+Y = data.iloc[:, -1].values         # -1 means last column
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(12 , activation="sigmoid"),
+    tf.keras.layers.Dense(20 , activation="relu"),
+    tf.keras.layers.Dense(10 , activation="relu"),
+    tf.keras.layers.Dense(4, activation="softmax")  
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+              loss='sparse_categorical_crossentropy',
+              metrics = ['accuracy'])
+
+output = model.fit(X_train, Y_train, epochs=100)
+
+model.evaluate(X_test, Y_test)
+
+model.save('Snake-AI_model.h5')
