eca1504ca1261cbf3233ee5207c3f9bfe2ea7904
Mahdi Ahmadi Kamal <909stars@gmail.com>
2024-07-12 23:04:04 +0430

    8th commit

---
+++ PyLearn7-ML_Mini_Project/PyLearn7-ML_Mini_Project_2/main_ai.py
@@ -0,0 +1,58 @@
+import arcade
+from snake import Snake
+from apple import Apple
+
+
+class Game(arcade.Window):
+    def __init__(self):
+        super().__init__(width=500, height=500, title="Super Snake V.1.0 🐍")
+        arcade.set_background_color(arcade.color.KHAKI)
+
+        self.snake = Snake(self)
+        self.food = Apple(self)
+        self.condition = ""
+            
+    def on_draw(self):
+        arcade.start_render()
+        self.snake.draw()
+        self.food.draw()
+        
+        arcade.draw_text(f"score: {self.snake.score}", self.width-100, 15, arcade.color.BLACK, 15)
+
+        if self.condition == "Game Over":
+            arcade.start_render()
+            arcade.draw_text("Game Over",self.width/5, self.height/2, arcade.color.BLACK, 45)
+            
+        arcade.finish_render()
+
+    def on_update(self, delta_time: float):
+        
+        self.snake.move_ai(self.food)
+
+        if self.snake.center_x > self.width or self.snake.center_x<0:
+            self.condition = "Game Over"
+            
+        if self.snake.center_y > self.height or self.snake.center_y<0:
+            self.condition = "Game Over"
+
+        if arcade.check_for_collision(self.snake, self.food):
+            self.snake.eat(self.food)
+
+            if self.snake.score <= 0:
+                self.condition = "Game Over"
+            
+            self.food = Apple(self)
+
+        # for part in self.snake.body:
+        #     if self.snake.center_x == part["x"] and self.snake.center_y == part["y"]:
+        #         self.condition = "Game Over"
+        
+    def on_key_release(self, symbol: int, modifiers: int):
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    arcade.run()
+
+
--- PyLearn7-ML_Mini_Project/PyLearn7-ML_Mini_Project_2/train.py
+++ PyLearn7-ML_Mini_Project/PyLearn7-ML_Mini_Project_2/train.py
@@ -13,13 +13,14 @@ Y = data.iloc[:, -1].values         # -1 means last column
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(12, activation='relu'),
-    tf.keras.layers.Dense(50, activation='relu'),
-    tf.keras.layers.Dense(30, activation='relu'),
-    tf.keras.layers.Dense(4, activation='softmax')
+    tf.keras.layers.Dense(13 , activation="relu"),
+    tf.keras.layers.Dense(300 , activation="relu"),
+    tf.keras.layers.Dense(200 , activation="relu"),
+    tf.keras.layers.Dense(300 , activation="relu"),
+    tf.keras.layers.Dense(4, activation="softmax")   
 ])
 
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
               loss= tf.keras.losses.sparse_categorical_crossentropy,
               metrics=['accuracy'])
 
